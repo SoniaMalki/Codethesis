8a70e851dd605120b22f12e15ccb63e55643dde0
Sonia Malki <sonia.malki@ulb.be>
2024-06-17 14:42:28 +0200

    modif creation conteneur

---
+++ ExperienceConfiguration/ExperiencesConfigurationBackend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Correct ownership of all files - to be executed as root before switching to springuser
+sudo chown -R springuser:springuser /home/springuser/app
+
+# Run Maven clean and package
+mvn clean package -DskipTests -e -X
+
+# Start the application
+java -jar target/*.jar

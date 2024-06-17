0327afc9df439f0f2f8957c29391cd546a0879ac
Sonia Malki <sonia.malki@ulb.be>
2024-06-17 14:58:31 +0200

    modif volume config files

--- ExperienceConfiguration/ExperiencesConfigurationBackend/entrypoint.sh
+++ ExperienceConfiguration/ExperiencesConfigurationBackend/entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Correct ownership of all files - to be executed as root before switching to springuser
 sudo chown -R springuser:springuser /home/springuser/app
+sudo chown -R springuser:springuser /home/springuser/config_files
 
 # Run Maven clean and package
 mvn clean package -DskipTests -e -X

8d76c8cc8a79bcc5f9d5dcef3a89939ebe47a7b4
Sonia Malki <sonia.malki@ulb.be>
2024-06-27 14:29:33 +0200

    ffdu et wfdu changé

--- modules/assignment/assignment_algorithms/ffdu.py
+++ modules/assignment/assignment_algorithms/ffdu.py
@@ -1,15 +1,8 @@
-import numpy
-import math
-import time
-
 class Ffdu:
     def __init__(self, taskset, number_of_cores):
         self.number_of_cores = number_of_cores
         self.taskset = taskset
-        self.period = self.taskset.period
-        self.wcet = self.taskset.wcet
         self.utilization = self.taskset.utilization
-        self.interference = self.taskset.interference
 
     def assign(self):
         taskset = sorted(range(len(self.utilization)), key=lambda k: self.utilization[k], reverse=True)
@@ -28,24 +21,22 @@ class Ffdu:
     def task_partition(self, task_in_core, taskset_not_assigned):
         successfully_assigned = 0
         task_not_assigned = []
-        for task_index in taskset_not_assigned:  
-            core_found = False
-            for core_index in range(self.number_of_cores):
-                if self.check_task_fit(task_index, core_index, task_in_core):
-                    task_in_core[core_index].append(task_index)
-                    successfully_assigned = 1
-                    core_found = True
-                    break
-            if not core_found:
+        for task_index in taskset_not_assigned:
+            core_index = self.find_first_fit_core(task_in_core, task_index)
+            if core_index is not None:
+                task_in_core[core_index].append(task_index)
+                successfully_assigned = 1
+            else:
                 task_not_assigned.append(task_index)
-                
         return task_in_core, task_not_assigned, successfully_assigned
 
-    def check_task_fit(self, task_index, core_index, task_in_core):
-        total_utilization = sum(
-            self.utilization[task] for task in task_in_core[core_index]
-        )
-        if total_utilization + self.utilization[task_index] <= 1:
-            return True
-        else:
-            return False
+    def find_first_fit_core(self, task_in_core, task_index):
+        """Trouve le premier cœur disponible (pour FFDU) et vérifie la limite d'utilisation."""
+        for core_index in range(self.number_of_cores):
+            total_utilization = sum(
+                self.utilization[task] for task in task_in_core[core_index]
+            )
+            # Vérifie si la tâche rentre dans la limite d'utilisation du cœur
+            if total_utilization + self.utilization[task_index] <= 1:
+                return core_index
+        return None
--- modules/assignment/assignment_algorithms/wfdu.py
+++ modules/assignment/assignment_algorithms/wfdu.py
@@ -1,16 +1,8 @@
-import numpy
-import math
-import time
-
 class Wfdu:
     def __init__(self, taskset, number_of_cores):
         self.number_of_cores = number_of_cores
         self.taskset = taskset
-        self.period = self.taskset.period
-        self.wcet = self.taskset.wcet
         self.utilization = self.taskset.utilization
-        self.interference = self.taskset.interference
-
 
     def assign(self):
         taskset = sorted(range(len(self.utilization)), key=lambda k: self.utilization[k], reverse=True)
@@ -39,7 +31,7 @@ class Wfdu:
         return task_in_core, task_not_assigned, successfully_assigned
 
     def find_worst_fit_core(self, task_in_core, task_index):
-        """Trouve le cœur avec l'utilisation la plus élevée (pour WFDU) et vérifie la limite d'utilisation."""
+        """Trouve le cœur avec l'utilisation la moins élevée (pour WFDU) et vérifie la limite d'utilisation."""
         min_utilization = 1
         worst_fit_core = None
         for core_index in range(self.number_of_cores):

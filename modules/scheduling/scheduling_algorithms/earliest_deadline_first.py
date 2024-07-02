bf0495fc5bf24c42e9cd0508f2adc220f0aa8635
Sonia Malki <sonia.malki@ulb.be>
2024-07-02 13:15:54 +0200

    modif de edf et job pour prendre en compte interférence (à implémenter)

--- modules/scheduling/scheduling_algorithms/earliest_deadline_first.py
+++ modules/scheduling/scheduling_algorithms/earliest_deadline_first.py
@@ -6,42 +6,57 @@ class EarliestDeadlineFirst:
         self.number_of_cores = number_of_cores
         self.job_list = [[] for _ in range(self.number_of_cores)] 
         self.ready_queue = [[] for _ in range(self.number_of_cores)] 
+        self.current_jobs = [None] * self.number_of_cores 
+        self.schedule_res = [[] for _ in range(self.number_of_cores)]
 
     def schedule(self):
         current_time = 0
-        schedule = [[] for _ in range(self.number_of_cores)]  
+        self.create_job_list(current_time=current_time)
 
+        while current_time < self.hyperperiod:
+            for core_index in range(self.number_of_cores):
+                self.update_ready_queue(core_index=core_index, current_time=current_time)
+                if not self.select_job(core_index=core_index, current_time=current_time):
+                    return self.schedule_res, 0  # Return 0 if deadline missed
+                self.check_interference(core_index=core_index, current_time=current_time)
+                self.execute_job(core_index=core_index, current_time=current_time)
+            current_time += 1
+
+        return self.schedule_res, 1  # Return schedule and success because no deadline missed at the end of Hyperperiod
+    
+    def create_job_list(self, current_time):
         for core_index in range(self.number_of_cores):
             for task_index in range(len(self.assignment[core_index])):
                 task = self.assignment[core_index][task_index]
                 task.create_jobs(start_time=current_time, finish_time=self.hyperperiod)
                 self.job_list[core_index].extend(task.job_list)
 
-        while current_time < self.hyperperiod:
-            for core_index in range(self.number_of_cores):
-                for job in self.job_list[core_index]:
-                    if job.arrival_time == current_time and not job.completed:
-                        self.ready_queue[core_index].append(job)
+    def update_ready_queue(self, core_index, current_time):
+        for job in self.job_list[core_index]:
+            if job.arrival_time == current_time and not job.completed:
+                self.ready_queue[core_index].append(job)
+        self.ready_queue[core_index].sort(key=lambda job: job.relative_deadline)
 
-                self.ready_queue[core_index].sort(key=lambda job: job.relative_deadline)
 
-                job_to_execute = None
-                for job in self.ready_queue[core_index]:
-                    if job.relative_deadline >= current_time + 1:
-                        job_to_execute = job
-                        break
-                    else:
-                        return schedule, 0  
-                    
-                if job_to_execute:
-                    schedule[core_index].append((current_time, job_to_execute.task_number, job_to_execute.job_identifier))
-                    job_to_execute.execute()
+    def select_job(self, core_index, current_time):
+        self.current_jobs[core_index] = None
+        for job in self.ready_queue[core_index]:
+            if job.relative_deadline >= current_time + 1:
+                self.current_jobs[core_index] = job  
+                break
+            else:
+                return False
+        return True
+    
+    def execute_job(self, core_index, current_time):
+        job_to_execute = self.current_jobs[core_index]
+        if job_to_execute:
+            self.schedule_res[core_index].append((current_time, job_to_execute.task_number, job_to_execute.job_identifier))
+            job_to_execute.execute()
+            if job_to_execute.completed:
+                self.ready_queue[core_index].remove(job_to_execute)
+        else:
+            self.schedule_res[core_index].append((current_time, None, None))
 
-                    if job_to_execute.completed:
-                        self.ready_queue[core_index].remove(job_to_execute)
-                else:
-                    schedule[core_index].append((current_time, None, None))
-
-            current_time += 1
-
-        return schedule, 1  
+    def check_interference(self, core_index, current_time):
+        pass
--- modules/taskset/job.py
+++ modules/taskset/job.py
@@ -1,41 +1,39 @@
 class Job:
     def __init__(self, task_number, job_identifier, wcet, relative_deadline, arrival_time, interference_factor):
-        # To identify job
         self.task_number = task_number
         self.job_identifier = job_identifier
-        # Fixed attributes
         self.wcet = wcet
         self.relative_deadline = relative_deadline
         self.arrival_time = arrival_time
-        self.interference_factor = interference_factor
-
-        # Changing attributes
-        self.execution_time = 0  
+        self.interference_factor = max(interference_factor)
+        
+        self.remaining_execution_time = self.wcet
         self.completed = False
+        self.interference_history = set()  # To keep track of which jobs have interfered
 
     def __repr__(self):
         return (
-            "Job("
-            f"task_number={self.task_number}, "
-            f"job_identifier={self.job_identifier}, "
-            f"wcet={self.wcet}, "
-            f"relative_deadline={self.relative_deadline}, "
-            f"arrival_time={self.arrival_time}, "
-            f"interference_factor={self.interference_factor}"
-            ")"
+            f"Job(task_number={self.task_number}, job_identifier={self.job_identifier}, "
+            f"wcet={self.wcet}, relative_deadline={self.relative_deadline}, "
+            f"arrival_time={self.arrival_time}, interference_factor={self.interference_factor})"
         )
 
     def __str__(self):
-        res = (f"Job identifier: {self.job_identifier}\n"
-               f"Arrival time: {self.arrival_time}\n"
-               f"WCET: {self.execution_time}\n"
-               f"Relative Deadline: {self.relative_deadline}\n"
-               f"Completed: {self.completed}\n"
-               f"Remaining Execution Time: {self.execution_time}")
-        return res
+        return (f"Job identifier: {self.job_identifier}\n"
+                f"Arrival time: {self.arrival_time}\n"
+                f"WCET: {self.wcet}\n"
+                f"Relative Deadline: {self.relative_deadline}\n"
+                f"Completed: {self.completed}\n"
+                f"Remaining Execution Time: {self.remaining_execution_time}\n"
+                f"Interference Received: {self.interference_history}")
 
     def execute(self):
-        if self.execution_time < self.wcet:
-            self.execution_time += 1
-            if self.execution_time >= self.wcet:
+        if self.remaining_execution_time > 0:
+            self.remaining_execution_time -= 1
+            if self.remaining_execution_time <= 0:
                 self.completed = True
+
+    def apply_interference(self, interfering_job_id):
+        if interfering_job_id not in self.interference_history:
+            self.remaining_execution_time += 1  # Assuming a fixed delay for simplicity
+            self.interference_history.add(interfering_job_id)

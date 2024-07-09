40b54f1ff0536ff18a466c176be23e72830400ba
Sonia Malki <sonia.malki@ulb.be>
2024-07-09 15:22:23 +0200

    verifié jusque contraint 18 (a faire 19)

--- modules/scheduling/scheduling_algorithms/rhma.py
+++ modules/scheduling/scheduling_algorithms/rhma.py
@@ -211,29 +211,33 @@ class Rhma:
             # Constraint 18
             for i in range(len(self.taskset)):
                 for j in range(self.number_of_cores):
-                    if self.o_i_j[i][j] == 1:
+                    if len(self.S_i_h[i][h]) > 0:
                         left_side = lpSum(x[i, a, j, t] for a in self.S_i_h[i][h]
                                           for t in self.R_i_a_h[i][a][h])
 
                         right_side = len(
                             self.S_i_h[i][h]) * self.taskset.wcet[i] * self.o_i_j[i][j]
+
                         right_side += lpSum(m[i, a, k, b] * self.taskset.interference[k] * self.o_i_j[i][j]
-                                            for k in range(len(self.taskset)) if k != i
+                                            for k in range(len(self.taskset)) if k != i and self.o_i_j[i][j] != self.o_i_j[k][j] and len(self.S_i_h[k][h]) > 0 and self.taskset.interference[k] > 0 and self.taskset.interference[i] > 0
                                             for a in self.S_i_h[i][h]
                                             for b in self.S_i_h[k][h])
+
                         prob += left_side == right_side
 
             # Constraint 19
             for i in range(len(self.taskset)):
                 for a in self.S_i_h[i][h]:
                     for j in range(self.number_of_cores):
-                        if self.o_i_j[i][j] == 1:
-
+                        if len(self.S_i_h[i][h]) > 0:
                             left_side = lpSum(x[i, a, j, t]
                                               for t in self.R_i_a_h[i][a][h])
 
                             right_side = self.taskset.wcet[i] * \
                                 self.o_i_j[i][j]
+
+                            print(right_side)
+                            time.sleep(2)
                             right_side += lpSum(m[i, a, k, b] * self.taskset.interference[k] * self.o_i_j[i][j]
                                                 for k in range(len(self.taskset)) if k != i
                                                 for b in self.S_i_h[k][h])

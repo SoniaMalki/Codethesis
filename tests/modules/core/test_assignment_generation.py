89239377832197a6c6d949874880e5b040821fb5
Sonia Malki <sonia.malki@ulb.be>
2024-07-15 16:25:30 +0200

    test tournent mais probleme quand je lance tout en meme temps

--- tests/modules/core/test_assignment_generation.py
+++ tests/modules/core/test_assignment_generation.py
@@ -9,10 +9,12 @@ from modules.taskset.taskset_set_generator import TasksetSetGenerator
 from modules.taskset.taskset_set_manual import TasksetSetManual
 # Configuration initiale des tests
 
-random.seed(42)
 np.random.seed(42)
+random.seed(42)  # Setting a seed for reproducibility
 
-# Taskset 1
+assignment_methods = ["WFDU", "FFDU", "BFDU", "Wmin"]
+citta_criteria_list = ["utilization_descending"]
+experiences = ["manual_1", "generate_1"]
 
 
 def prepare_input_data_manual_1(assignment_method, citta_criteria=""):
@@ -40,19 +42,21 @@ def prepare_input_data_manual_1(assignment_method, citta_criteria=""):
     return taskset_action, taskset_id, taskset_parameters, assignment_parameters
 
 
-def create_expected_assignment_output_manual_1(assignment_method):
+def create_expected_assignment_output_manual_1(assignment_method, citta_criteria):
+    if assignment_method == "CITTA":
+        assignment_method = ("CITTA",  citta_criteria)
     expected_assignment = {
         "WFDU": [(1, [[0, 1], [3, 2]])],
         "FFDU": [(1, [[0, 3, 1], [2]])],
         "BFDU": [(1, [[0, 3, 1], [2]])],
-        "CITTA": [(1, [[0, 3], [1, 2]])],
+        ("CITTA", "utilization_descending"): [(1, [[0, 3], [1, 2]])],
         "Wmin": [(1, [[0], [1, 2, 3]])]
     }
 
     return expected_assignment[assignment_method]
 
 
-def prepare_input_data_generate_1(assignment_method, citta_critera=""):
+def prepare_input_data_generate_1(assignment_method, citta_criteria=""):
     taskset_action = "generate"
     taskset_id = "taskset_generate_1"
     taskset_parameters = {
@@ -65,23 +69,39 @@ def prepare_input_data_generate_1(assignment_method, citta_critera=""):
     assignment_parameters = {
         "assignment_id": "assignment_generate_1",
         "taskset_id": "taskset_generate_1",
-        "citta_criteria": [citta_critera],
+        "citta_criteria": [citta_criteria],
         "assignment_method": [assignment_method],
         "number_of_cores": 2
     }
     return taskset_action, taskset_id, taskset_parameters, assignment_parameters
 
 
-def create_expected_assignment_output_generate_1(assignment_method):
+def create_expected_assignment_output_generate_1(assignment_method, citta_criteria):
+    if assignment_method == "CITTA":
+        assignment_method = ("CITTA",  citta_criteria)
     expected_assignment = {
         "WFDU": [(1, [[2, 0], [3, 1]])],
         "FFDU": [(1, [[2, 3, 1, 0], []])],
         "BFDU": [(1, [[2, 3, 1, 0], []])],
-        "CITTA": [(1, [[2, 1, 0], [3]])],
+        ("CITTA", "utilization_descending"): [(1, [[2, 1, 0], [3]])],
         "Wmin": [(1, [[], [0, 1, 2, 3]])]
     }
 
     return expected_assignment[assignment_method]
+
+
+def prepare_input_data(experience, assignment_method, citta_criteria=""):
+    if experience == "manual_1":
+        return prepare_input_data_manual_1(assignment_method=assignment_method, citta_criteria=citta_criteria)
+    elif experience == "generate_1":
+        return prepare_input_data_generate_1(assignment_method=assignment_method, citta_criteria=citta_criteria)
+
+
+def prepare_output_data(experience, assignment_method, citta_criteria=""):
+    if experience == "manual_1":
+        return create_expected_assignment_output_manual_1(assignment_method=assignment_method, citta_criteria=citta_criteria)
+    elif experience == "generate_1":
+        return create_expected_assignment_output_generate_1(assignment_method=assignment_method, citta_criteria=citta_criteria)
 
 
 def create_taskset_manual(taskset_id, taskset_parameters):
@@ -131,30 +151,17 @@ def verify_assignment(assignment, expected_assignment):
         assert result.assignment == exp_ass[1]
 
 
-@ pytest.mark.parametrize("input_data, expected_assigment", [
-    (prepare_input_data_manual_1("WFDU"),
-     create_expected_assignment_output_manual_1("WFDU")),
-    (prepare_input_data_manual_1("FFDU"),
-     create_expected_assignment_output_manual_1("FFDU")),
-    (prepare_input_data_manual_1("BFDU"),
-     create_expected_assignment_output_manual_1("BFDU")),
-    (prepare_input_data_manual_1("CITTA", "utilization_descending"),
-     create_expected_assignment_output_manual_1("CITTA")),
-    (prepare_input_data_manual_1("Wmin"),
-     create_expected_assignment_output_manual_1("Wmin")),
-    (prepare_input_data_generate_1("WFDU"),
-     create_expected_assignment_output_generate_1("WFDU")),
-    (prepare_input_data_generate_1("FFDU"),
-     create_expected_assignment_output_generate_1("FFDU")),
-    (prepare_input_data_generate_1("BFDU"),
-     create_expected_assignment_output_generate_1("BFDU")),
-    (prepare_input_data_generate_1("CITTA", "utilization_descending"),
-     create_expected_assignment_output_generate_1("CITTA")),
-    (prepare_input_data_generate_1("Wmin"),
-     create_expected_assignment_output_generate_1("Wmin"))
-])
-def test_assignment(input_data, expected_assigment):
+# Paramétrage des tests avec pytest.mark.parametrize
+@pytest.mark.parametrize("assignment_method", assignment_methods, ids=lambda method: f"{method}")
+@pytest.mark.parametrize("citta_criteria", citta_criteria_list, ids=lambda criteria: f"{criteria}")
+@pytest.mark.parametrize("experience", experiences, ids=lambda exp: f"{exp}")
+def test_assignment(assignment_method, citta_criteria, experience):
+    input_data = prepare_input_data(
+        experience, assignment_method, citta_criteria)
     taskset_action, taskset_id, taskset_parameters, assignment_parameters = input_data
+    expected_assignment = prepare_output_data(
+        experience=experience, assignment_method=assignment_method, citta_criteria=citta_criteria)
+
     if taskset_action == "manual":
         taskset = create_taskset_manual(taskset_id, taskset_parameters)
     elif taskset_action == "generate":
@@ -162,4 +169,4 @@ def test_assignment(input_data, expected_assigment):
     else:
         print("Invalid parameter")
     assignment = create_assignment(taskset, assignment_parameters)
-    verify_assignment(assignment, expected_assigment)
+    verify_assignment(assignment, expected_assignment)

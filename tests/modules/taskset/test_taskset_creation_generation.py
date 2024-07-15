6e569a18637ef703760729ac7f7b7c4fe2ff8806
Sonia Malki <sonia.malki@ulb.be>
2024-07-15 12:57:18 +0200

    création 1 test pour generator avec seed 42

---
+++ tests/modules/taskset/test_taskset_creation_generation.py
@@ -0,0 +1,136 @@
+import numpy as np
+import pytest
+import random
+from modules.taskset.taskset_set_generator import TasksetSetGenerator
+np.random.seed(42)
+random.seed(42)  # Setting a seed for reproducibility
+
+# Test N1
+
+
+def prepare_input_data():
+    """Prepare and return input data for the test."""
+    return {
+        "taskset_id": "taskset_generate_test",
+        "taskset_repetition": 1,
+        "probability_factors": [0.1],
+        "max_utilizations": [0.2],
+        "tasks_per_taskset": 2,
+        "interference_factors": [0.2]
+    }
+
+
+def expected_taskset_set_structure():
+    """Prepare and return expected TasksetSet structure for the test."""
+    return {
+        'taskset_id': "taskset_generate_test",
+        'wcet': np.array([[10, 1]]),
+        'deadline': np.array([[43, 17]]),
+        'period': np.array([[55, 55]]),
+        'interference': np.array([[[0, 0],
+                                   [0, 0],
+                                   ]]),
+        'utilization': np.array([[0.19507143, 0.00492857]])
+    }
+
+
+def expected_taskset_structure():
+    """Prepare and return expected Taskset structure for the test."""
+    return [{
+        'taskset_number': 0,
+        'wcet': np.array([10, 1]),
+        'deadline': np.array([43, 17]),
+        'period': np.array([55, 55]),
+        'interference': np.array([[0, 0],
+                                  [0, 0],
+                                  ]),
+        'utilization': np.array([0.19507143, 0.00492857]),
+        'hyperperiod': 55,
+        'N': np.array([1, 1]),
+        'activation': [np.array([1]), np.array([1])],
+        'absolute_deadline': [{1: 44}, {1: 18}],
+
+    }]
+
+
+def expected_task_structure():
+    """Prepare and return expected Task structures for the test."""
+    return [
+        {'task_number': 0, 'wcet': 10, 'deadline': 43, 'period': 55, 'interference': [
+            0, 0], 'utilization': 0.19507143, 'absolute_deadline': {1: 44}},
+        {'task_number': 1, 'wcet': 1, 'deadline': 17, 'period': 55,
+            'interference': [0, 0], 'utilization': 0.00492857, 'absolute_deadline': {1: 18}}
+    ]
+
+# Test creation
+def create_taskset(data):
+    """Create and return a Taskset object using provided data."""
+    generator = TasksetSetGenerator(
+        taskset_id=data['taskset_id'],
+        taskset_repetition=data['taskset_repetition'],
+        list_of_probability_factors=data['probability_factors'],
+        list_of_max_utilization=data['max_utilizations'],
+        tasks_per_taskset=data['tasks_per_taskset'],
+        list_of_interference_factors=data['interference_factors']
+    )
+    taskset_set = generator.generate_taskset_set()
+    return taskset_set
+
+
+def verify_taskset_set_attributes(taskset_set, expected_taskset_set):
+    """Verify all expected attributes of the TasksetSet object."""
+    assert taskset_set.taskset_id == expected_taskset_set['taskset_id']
+    assert np.array_equal(taskset_set.wcet, expected_taskset_set['wcet'])
+    assert np.array_equal(taskset_set.deadline,
+                          expected_taskset_set['deadline'])
+    assert np.array_equal(taskset_set.period, expected_taskset_set['period'])
+    assert np.array_equal(taskset_set.interference,
+                          expected_taskset_set['interference'])
+    assert np.allclose(taskset_set.utilization,
+                       expected_taskset_set['utilization'], rtol=1e-04)
+
+
+def verify_taskset_attributes(taskset_set, expected_taskset):
+    """Verify all expected attributes of the Taskset object."""
+    for taskset, exp_taskset in zip(taskset_set.taskset_list, expected_taskset):
+        assert taskset.taskset_number == exp_taskset['taskset_number']
+        assert np.array_equal(taskset.wcet, exp_taskset['wcet'])
+        assert np.array_equal(taskset.deadline, exp_taskset['deadline'])
+        assert np.array_equal(taskset.period, exp_taskset['period'])
+        assert np.array_equal(taskset.interference,
+                              exp_taskset['interference'])
+        assert np.allclose(taskset.utilization,
+                           exp_taskset['utilization'], rtol=1e-04)
+        assert taskset.hyperperiod == exp_taskset['hyperperiod']
+        assert np.array_equal(taskset.N, exp_taskset['N'])
+        assert all(np.array_equal(a, b)
+                   for a, b in zip(taskset.activation, exp_taskset['activation']))
+        assert all(np.array_equal(a, b)
+                   for a, b in zip(taskset.absolute_deadline, exp_taskset['absolute_deadline']))
+
+
+def verify_task_attributes(taskset_set, expected_tasks):
+    """Verify all expected attributes for each Task in the Taskset."""
+    for taskset in taskset_set.taskset_list:
+        for task, exp_task in zip(taskset.task_list, expected_tasks):
+            assert task.task_number == exp_task['task_number']
+            assert task.wcet == exp_task['wcet']
+            assert task.deadline == exp_task['deadline']
+            assert task.period == exp_task['period']
+            assert np.array_equal(task.interference,
+                                  exp_task['interference'])
+            assert np.isclose(task.utilization,
+                              exp_task['utilization'], rtol=1e-04)
+            assert task.absolute_deadline == exp_task['absolute_deadline']
+
+
+@pytest.mark.parametrize("input_data, expected_taskset_set, expected_taskset, expected_tasks", [
+    (prepare_input_data(), expected_taskset_set_structure(),
+     expected_taskset_structure(), expected_task_structure())
+])
+def test_taskset_creation(input_data, expected_taskset_set, expected_taskset, expected_tasks):
+    """Main test function to test taskset creation and attribute validation."""
+    taskset_set = create_taskset(input_data)
+    verify_taskset_set_attributes(taskset_set, expected_taskset_set)
+    verify_taskset_attributes(taskset_set, expected_taskset)
+    verify_task_attributes(taskset_set, expected_tasks)

3fb6f7692fcfeb4f656ef60ca9270ac507f6dac2
Sonia Malki <sonia.malki@ulb.be>
2024-07-15 14:45:16 +0200

    tout les assignation algo sont à jour pour 1 taskset, je vais en ajouter d'autres

--- tests/modules/core/test_assignment_generation.py
+++ tests/modules/core/test_assignment_generation.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from modules.assignment.assignment_set import AssignmentSet
 from modules.assignment.assignment_generator import AssignmentGenerator
@@ -7,7 +8,7 @@ from modules.taskset.taskset_set_manual import TasksetSetManual
 # Configuration initiale des tests
 
 
-def prepare_input_data_1():
+def prepare_input_data_1(assignment_method):
     taskset_parameters = {
         "taskset_id": "taskset_17",
         "wcet_list": [2, 3, 2, 3],
@@ -25,7 +26,7 @@ def prepare_input_data_1():
         "assignment_id": "assignment_17",
         "taskset_id": "taskset_17",
         "citta_criteria": ["utilization_descending"],
-        "assignment_method": ["WFDU"],
+        "assignment_method": [assignment_method],
         "number_of_cores": 2
     }
     return taskset_parameters, assignment_parameters
@@ -35,12 +36,30 @@ def create_expected_assignment_input_1_wfdu():
     return [(1, [[0, 1], [3, 2]])]
 
 
+def create_expected_assignment_input_1_ffdu():
+    return [(1, [[0, 3, 1], [2]])]
+
+
+def create_expected_assignment_input_1_bfdu():
+    return [(1, [[0, 3, 1], [2]])]
+
+
+def create_expected_assignment_input_1_citta():
+    return [(1, [[0, 3], [1, 2]])]
+
+
+def create_expected_assignment_input_1_wmin():
+    return [(1, [[0], [1, 2, 3]])]
+
+
 def create_taskset(taskset_parameters):
     generator = TasksetSetManual(**taskset_parameters)
     return generator.create_taskset_set()
 
 
 def create_assignment(taskset, assignment_parameters):
+    if assignment_parameters["assignment_method"][0].lower() == 'wmin':
+        taskset = shape_interference(taskset)
     generator = AssignmentGenerator(
         taskset_set_obj=taskset,
         taskset_id=assignment_parameters["taskset_id"],
@@ -53,6 +72,21 @@ def create_assignment(taskset, assignment_parameters):
     return assignment
 
 
+def shape_interference(taskset_set_obj):
+    new_interference = []
+    for taskset in taskset_set_obj:
+        taskset_interference = np.max(taskset.interference, axis=1)
+        # Modif au niveau Taskset Set
+        new_interference.append(taskset_interference)
+        taskset.interference = taskset_interference  # Modif au niveau Taskset
+        for task_index, task in enumerate(taskset):
+            # Modif au niveau Task
+            task.interference = taskset_interference[task_index]
+
+    taskset_set_obj.interference = np.array(new_interference)
+    return taskset_set_obj
+
+
 def verify_assignment(assignment, expected_assignment):
     for result, exp_ass in zip(assignment, expected_assignment):
         # Vérification des résultats
@@ -61,7 +95,11 @@ def verify_assignment(assignment, expected_assignment):
 
 
 @pytest.mark.parametrize("input_data, expected_assigment", [
-    (prepare_input_data_1(), create_expected_assignment_input_1_wfdu())
+    (prepare_input_data_1("WFDU"), create_expected_assignment_input_1_wfdu()),
+    (prepare_input_data_1("FFDU"), create_expected_assignment_input_1_ffdu()),
+    (prepare_input_data_1("BFDU"), create_expected_assignment_input_1_bfdu()),
+    (prepare_input_data_1("CITTA"), create_expected_assignment_input_1_citta()),
+    (prepare_input_data_1("Wmin"), create_expected_assignment_input_1_wmin())
 ])
 def test_assignment(input_data, expected_assigment):
     taskset_parameters, assignment_parameters = input_data

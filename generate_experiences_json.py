83bb574dc9f82d6108cf5d4770376f9d55c54cb9
Sonia Malki <sonia.malki@ulb.be>
2024-07-24 18:08:14 +0200

    generation dans des fichiers séparés (pour faciliter parallélisation hydra)

--- generate_experiences_json.py
+++ generate_experiences_json.py
@@ -1,44 +1,29 @@
 import json
 import itertools
 
-def generate_experiences():
-    experiences = {}
 
-    # Paramètres de Taskset
-    taskset_repetitions = [1] #, 2, 4, 8, 16]
-    tasks_per_taskset = [10] #, 20]
-    interference_factors = [0.2] #, 0.8]
-    probability_factors = [0.1] #, 0.4]
-    max_utilization_factors = [0.2] #, 0.4, 0.6, 0.8, 1.0] # Facteurs d'utilisation
-    deadline_options = ["eq_period"] #, "leq_period"]
-    max_hyperperiods = [10] #, 100, 1000]
-    max_primes = [2] #, 3, 5, 7, 11, 13, 17, 19, 23]
-    gen_limit_exponents = [2] #, 3, 4, 5]
+def generate_tasksets():
+    tasksets = {}
 
-    # Paramètres d'Assignment
-    assignment_methods = ["CITTA"] #, "WorstFitAssigner", "FirstFitAssigner", "BestFitAssigner", "Wmin"]
-    sorting_criteria = ["wcet_ascending"] #, "wcet_descending", "period_ascending",
-                       #"period_descending", "utilization_ascending", "utilization_descending", 
-                       #"execution_slack_ascending", "execution_slack_descending", "random_order"]
-    number_of_cores_list = [2] #, 4, 8]
-    solving_time_limit_milp_assignment = [2] #, 5, 10] 
+    taskset_repetitions = [1]
+    tasks_per_taskset = [10]
+    interference_factors = [0.2]
+    probability_factors = [0.1]
+    max_utilization_factors = [0.2]
+    deadline_options = ["eq_period"]
+    max_hyperperiods = [10]
+    max_primes = [2]
+    gen_limit_exponents = [2]
+    number_of_cores_list = [2]
 
-    # Paramètres de Scheduling
-    scheduling_algorithms = ["EarliestDeadlineFirst"] #, "EarliestDeadlineFirstVariant1", "EarliestDeadlineFirstVariant2",
-                             #"DeadlineMonotonic", "DeadlineMonotonicVariant1", "DeadlineMonotonicVariant2", 
-                             #"CombinedScheduler", "Rhma"]
-    non_preemption_time_variant2_options = ["number_of_tasks"] #, "wcet_of_tasks", "system_utilization"]
-    solving_time_limit_milp_scheduling = [1] #, 2, 5]  
-
-    # Générer les combinaisons de paramètres pour Taskset
     taskset_index = 1
     for repetition, tasks, interference, probability, util_factor, deadline, hyperperiod, prime, exponent in itertools.product(
         taskset_repetitions, tasks_per_taskset, interference_factors, probability_factors, max_utilization_factors,
         deadline_options, max_hyperperiods, max_primes, gen_limit_exponents
     ):
-        for cores in number_of_cores_list:  # Boucle sur les nombres de cœurs
+        for cores in number_of_cores_list:
             taskset_id = f"taskset_generate_{taskset_index}_c{cores}"
-            experiences[taskset_id] = {
+            tasksets[taskset_id] = {
                 "taskset": {
                     "action": "generate",
                     "taskset_id": taskset_id,
@@ -47,7 +32,7 @@ def generate_experiences():
                         "tasks_per_taskset": tasks,
                         "interference_factor": interference,
                         "probability_factor": probability,
-                        "max_utilization": util_factor * cores,  # Utilisation liée au nombre de cœurs
+                        "max_utilization": util_factor * cores,
                         "taskset_options": {
                             "deadline_option": deadline,
                             "max_hyperperiod": hyperperiod,
@@ -64,62 +49,86 @@ def generate_experiences():
                 }
             }
             taskset_index += 1
+    return tasksets
 
-    # Ouvrir chaque taskset et générer les combinaisons de paramètres pour Assignment
+
+def generate_assignments(tasksets):
+    assignments = {}
+
+    assignment_methods = ["CITTA"]
+    sorting_criteria = ["wcet_ascending"]
+    solving_time_limit_milp_assignment = [2]
+
     assignment_index = 1
-    # Correction : Itérer sur une copie des clés du dictionnaire
-    for taskset_key in list(experiences.keys()):  
-        for method, sorting, cores, solving_time in itertools.product(
-            assignment_methods, sorting_criteria, number_of_cores_list, solving_time_limit_milp_assignment
+    for taskset_key, taskset_data in tasksets.items():
+        cores = int(taskset_key.split('_c')[1])
+        for method, sorting, solving_time in itertools.product(
+                assignment_methods, sorting_criteria, solving_time_limit_milp_assignment
         ):
-            if cores == int(taskset_key.split('_c')[1]): # Vérification du nombre de cœurs
-                assignment_id = f"assignment_generate_{assignment_index}_c{cores}"
-                experiences[assignment_id] = {
-                    "taskset": {
-                        "action": "open",
-                        "taskset_id": taskset_key
-                    },
-                    "assignment": {
-                        "action": "generate",
-                        "assignment_id": assignment_id,
-                        "taskset_id": taskset_key,
-                        "parameters": {
-                            "sorting_criterion": sorting,
-                            "assignment_method": method,
-                            "number_of_cores": cores,
-                            "assignment_options": {
-                                "solving_time_limit_MILP": solving_time
-                            }
-                        }
-                    },
-                    "scheduling": {
-                        "action": "none"
-                    }
-                }
-                assignment_index += 1
-
-    # Ouvrir chaque assignment et générer les combinaisons de paramètres pour Scheduling
-    scheduling_index = 1
-    # Correction : Itérer sur une copie des clés du dictionnaire
-    for assignment_key in list(experiences.keys())[(len(experiences) // 2) + 1:]:  
-        for algorithm, non_preemption, solving_time in itertools.product(
-            scheduling_algorithms, non_preemption_time_variant2_options, solving_time_limit_milp_scheduling
-        ):
-            scheduling_id = f"scheduling_generate_{scheduling_index}_c{experiences[assignment_key]['assignment']['number_of_cores']}"
-            experiences[scheduling_id] = {
+            assignment_id = f"assignment_generate_{assignment_index}_c{cores}"
+            assignments[assignment_id] = {
                 "taskset": {
                     "action": "open",
-                    "taskset_id": experiences[assignment_key]["assignment"]["taskset_id"]
+                    "taskset_id": taskset_key,
+                    "parameters": {
+                        "none": "none"
+                    }
+                },
+                "assignment": {
+                    "action": "generate",
+                    "assignment_id": assignment_id,
+                    "taskset_id": taskset_key,
+                    "parameters": {
+                        "sorting_criterion": sorting,
+                        "assignment_method": method,
+                        "number_of_cores": cores,
+                        "assignment_options": {
+                            "solving_time_limit_MILP": solving_time
+                        }
+                    }
+                },
+                "scheduling": {
+                    "action": "none"
+                }
+            }
+            assignment_index += 1
+    return assignments
+
+
+def generate_schedulings(assignments):
+    schedulings = {}
+
+    scheduling_algorithms = ["EarliestDeadlineFirst"]
+    non_preemption_time_variant2_options = ["number_of_tasks"]
+    solving_time_limit_milp_scheduling = [1]
+
+    scheduling_index = 1
+    for assignment_key, assignment_data in assignments.items():
+        for algorithm, non_preemption, solving_time in itertools.product(
+                scheduling_algorithms, non_preemption_time_variant2_options, solving_time_limit_milp_scheduling
+        ):
+            cores = assignment_data["assignment"]["parameters"]["number_of_cores"]
+            scheduling_id = f"scheduling_generate_{scheduling_index}_c{cores}"
+            schedulings[scheduling_id] = {
+                "taskset": {
+                    "action": "open",
+                    "taskset_id": assignment_data["taskset"]["taskset_id"],
+                    "parameters": {
+                        "none": "none"
+                    }
                 },
                 "assignment": {
                     "action": "open",
                     "assignment_id": assignment_key,
-                    "taskset_id": experiences[assignment_key]["assignment"]["taskset_id"]
+                    "taskset_id": assignment_data["taskset"]["taskset_id"],
+                    "parameters": {
+                        "none": "none"
+                    }
                 },
                 "scheduling": {
                     "action": "generate",
                     "scheduling_id": scheduling_id,
-                    "taskset_id": experiences[assignment_key]["assignment"]["taskset_id"],
+                    "taskset_id": assignment_data["taskset"]["taskset_id"],
                     "assignment_id": assignment_key,
                     "parameters": {
                         "scheduling_algorithm": algorithm,
@@ -131,10 +140,18 @@ def generate_experiences():
                 }
             }
             scheduling_index += 1
+    return schedulings
 
-    return experiences
 
-# Générer le fichier experiences.json
-experiences = generate_experiences()
-with open("./config_files/experiences.json", "w") as f:
-    json.dump(experiences, f, indent=4)
+# Générer les fichiers JSON
+tasksets = generate_tasksets()
+with open("./config_files/tasksets.json", "w") as f:
+    json.dump(tasksets, f, indent=4)
+
+assignments = generate_assignments(tasksets)
+with open("./config_files/assignments.json", "w") as f:
+    json.dump(assignments, f, indent=4)
+
+schedulings = generate_schedulings(assignments)
+with open("./config_files/schedulings.json", "w") as f:
+    json.dump(schedulings, f, indent=4)

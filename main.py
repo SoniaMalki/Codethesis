585311fc7af4e30cace9c1f4feffffff2e159e2e
Sonia Malki <sonia.malki@ulb.be>
2024-07-24 18:11:57 +0200

    main peut accepter les nouveaux format expériences

--- main.py
+++ main.py
@@ -10,27 +10,27 @@ from modules.core.experience_loader import ExperienceLoader
 current_path = Path(__file__).parent
 
 
-def main(experience_parameter_index="1"):
+def main(experience_parameter_key="taskset_generate_1_c2"):
     """
-    Main function to run an experiment based on the experience.json configuration.
+    Main function to run an experiment based on the configuration files.
 
     Args:
-        experience_parameter_index (str, optional): The index of the experience to load from experience.json. Defaults to "1".
+        experience_parameter_key (str, optional): The key of the experience to load. Defaults to "taskset_generate_1_c2".
     """
 
     # Load the experience
     experience_loader = ExperienceLoader(Path(__file__).parent)
-    experience = experience_loader.load("config_files/experiences.json", experience_parameter_index)
+    experience = experience_loader.load(experience_parameter_key)
 
     # Process the experience
     experience.process()
 
 
-
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        experience_parameter_index = sys.argv[1]
-        main(experience_parameter_index)
+        experience_parameter_key = sys.argv[1]
+        main(experience_parameter_key)
     else:
-        print(f"No experience parameter index given. Defaulting to index 1.")
+        print(
+            f"No experience parameter key given. Defaulting to key 'taskset_generate_1_c2'.")
         main()

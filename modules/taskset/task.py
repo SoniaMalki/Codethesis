5bc9a65ecb74cc0ac22b63f28722518a3f0e2f27
Sonia Malki <sonia.malki@ulb.be>
2024-07-25 10:48:30 +0200

    mise a jour repr str pour task

--- modules/taskset/task.py
+++ modules/taskset/task.py
@@ -17,15 +17,11 @@ class Task:
         self.job_list = []
 
     def __repr__(self):
-        return ("Task("
-                f"task_number={self.task_number}, "
-                f"wcet={self.wcet}, "
-                f"deadline={self.deadline}, "
-                f"period={self.period}, "
-                f"interference={self.interference}, "
-                f"utilization={self.utilization}"
-                ")"
-                )
+        return (
+            f"Task(task_number={self.task_number}, wcet={self.wcet}, deadline={self.deadline}, "
+            f"period={self.period}, interference={self.interference}, single_interference={self.single_interference}, "
+            f"utilization={self.utilization}, absolute_deadline={self.absolute_deadline})"
+        )
 
     def __len__(self):
         return len(self.job_list)
@@ -40,19 +36,18 @@ class Task:
         return self.job_list[i]
 
     def __str__(self):
-        res = (f"Task number: {self.task_number}\n"
-               f"WCET: {self.wcet}\n"
-               f"Deadline: {self.deadline}\n"
-               f"Period: {self.period}\n"
-               f"Interference: {self.interference}\n"
-               f"Utilization: {self.utilization}\n")
-        job_str = "Jobs of the Task: "
-        for elem in self.job_list:
-            job_str = job_str + "\n" + str(elem)
-        if len(self.job_list) == 0:
-            job_str = job_str + "[]"
-        res = res + job_str
-        return res
+        job_str = "\n".join(repr(job) for job in self.job_list)
+        return (
+            f"Task Number: {self.task_number}\n"
+            f"WCET: {self.wcet}\n"
+            f"Deadline: {self.deadline}\n"
+            f"Period: {self.period}\n"
+            f"Interference: {self.interference}\n"
+            f"Single Interference: {self.single_interference}\n"
+            f"Utilization: {self.utilization}\n"
+            f"Absolute Deadline: {self.absolute_deadline}\n"
+            f"Jobs: \n{job_str}"
+        )
 
     def create_jobs(self, start_time, end_time):
         self.job_list = []

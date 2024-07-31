12d53bbccd404420bd3a518b05e1b560a3374373
Sonia Malki <sonia.malki@ulb.be>
2024-07-31 19:07:51 +0200

    retrieve amélioré

--- other_files/retrieve_results.sh
+++ other_files/retrieve_results.sh
@@ -11,13 +11,25 @@ DIRECTORIES=(
 )
 
 for dir in "${DIRECTORIES[@]}"; do
-    RSYNC_CMD="rsync -av ${SOURCE}${dir}/ ${DESTINATION}${dir}/"
-    echo "Executing: $RSYNC_CMD"
-    eval $RSYNC_CMD
-
+    # Vérifier l'existence du répertoire source sur le serveur distant
+    ssh lemaitre4 "test -d ~/Codethesis/$dir"
     if [ $? -eq 0 ]; then
-        echo "Transfer of ${dir} complete!"
+        # Créer le répertoire de destination s'il n'existe pas
+        if [ ! -d "${DESTINATION}${dir}" ]; then
+            echo "Creating directory ${DESTINATION}${dir}"
+            mkdir -p "${DESTINATION}${dir}"
+        fi
+
+        RSYNC_CMD="rsync -av ${SOURCE}${dir}/ ${DESTINATION}${dir}/"
+        echo "Executing: $RSYNC_CMD"
+        eval $RSYNC_CMD
+
+        if [ $? -eq 0 ]; then
+            echo "Transfer of ${dir} complete!"
+        else
+            echo "Transfer of ${dir} failed!"
+        fi
     else
-        echo "Transfer of ${dir} failed!"
+        echo "Source directory ${SOURCE}${dir} does not exist on the remote server. Skipping."
     fi
 done

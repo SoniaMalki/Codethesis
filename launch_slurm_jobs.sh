c3ed0412f9b5a79eaadf71b0ce9d1a00d8a76bde
Sonia Malki <sonia.malki@ulb.be>
2024-08-03 14:39:12 +0200

    creation sbatch pour launch slurm quand c'est trop gros

--- launch_slurm_jobs.sh
+++ launch_slurm_jobs.sh
@@ -12,14 +12,40 @@ experience_key=$1
 experience_dir="$script_dir/generation/$experience_key"
 master_dir="$experience_dir/slurm/master"
 
-
 #[ -d "$experience_dir" ] && rm -rf "$experience_dir"
 
-python3 "$script_dir/main.py" $experience_key generate_configs || { echo "Échec de la génération des configurations"; exit 1; }
-python3 "$script_dir/main.py" $experience_key generate_slurm_files || { echo "Échec de la génération des fichiers Slurm"; exit 1; }
+# Annoncer le début de la génération des configurations
+echo "Début de la génération des configurations pour $experience_key"
 
+python3 "$script_dir/main.py" $experience_key generate_configs
+if [ $? -ne 0 ]; then
+    echo "Échec de la génération des configurations"
+    exit 1
+else
+    echo "Génération des configurations réussie"
+fi
+
+# Annoncer le début de la génération des fichiers Slurm
+echo "Début de la génération des fichiers Slurm pour $experience_key"
+
+python3 "$script_dir/main.py" $experience_key generate_slurm_files
+if [ $? -ne 0 ]; then
+    echo "Échec de la génération des fichiers Slurm"
+    exit 1
+else
+    echo "Génération des fichiers Slurm réussie"
+fi
+
+# Vérifier et soumettre le job master.slurm
 if [ -f "$master_dir/master.slurm" ]; then
-    sbatch "$master_dir/master.slurm" || { echo "Échec de la soumission du job master.slurm"; exit 1; }
+    echo "Soumission du job master.slurm"
+    sbatch "$master_dir/master.slurm"
+    if [ $? -ne 0 ]; then
+        echo "Échec de la soumission du job master.slurm"
+        exit 1
+    else
+        echo "Job master.slurm soumis avec succès"
+    fi
 else
     echo "Erreur: $master_dir/master.slurm n'existe pas"
     exit 1

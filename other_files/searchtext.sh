dcd4a8489f0cec7a9d8fe8580a5ab4cd0ea022e0
Malki Sonia <sonia.malki@ulb.be>
2024-08-20 02:36:39 +0200

    meilleure recherche searchtext

--- other_files/searchtext.sh
+++ other_files/searchtext.sh
@@ -8,8 +8,28 @@ fi
 
 search_text="$1"
 
-# Find files containing the search text, excluding specified extensions
-files=$(grep -rl --exclude=*.pyc --exclude=*.class --exclude=*.json --exclude=*.pack --exclude=*.jar --exclude=esbuild --exclude=*.woff --exclude=*.zip --exclude=*.wasm --exclude=*.png "$search_text" .)
+# Ensure CODETHESIS environment variable is set
+if [ -z "$CODETHESIS" ]; then
+  echo "Environment variable CODETHESIS is not set."
+  exit 1
+fi
+
+# Define the directories to exclude
+exclude_dirs="error_handling/output_error|error_handling/filtered_output"
+
+# Find files containing the search text
+files=$(grep -rl "$search_text" "$CODETHESIS" \
+  --exclude-dir={.git,node_modules,cache,tmp,build,dist,.idea,.vscode,generation*} \
+  --exclude=*.pyc \
+  --exclude=*.class \
+  --exclude=*.json \
+  --exclude=*.pack \
+  --exclude=*.jar \
+  --exclude=*.woff \
+  --exclude=*.zip \
+  --exclude=*.wasm \
+  --exclude=*.png \
+  --exclude=*.git | grep -Ev "$exclude_dirs")
 
 # If no files found, exit
 if [ -z "$files" ]; then

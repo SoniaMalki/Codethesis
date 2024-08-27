341709ab238828de7b3063dc5b2528ca95e4693e
Malki Sonia <sonia.malki@ulb.be>
2024-08-27 17:08:45 +0200

    ajout index partout script aussi

--- other_files/populate_db.sh
+++ other_files/populate_db.sh
@@ -3,12 +3,21 @@
 # Définir le chemin de base pour les scripts et les données
 base_dir="$CODETHESIS"
 
-# Lire toutes les clés dans experience.json en conservant l'ordre d'apparition
-keys=$(jq -r 'to_entries|map(.key)|.[]' "$base_dir/experience.json")
+# Vérifier que l'argument index est fourni et qu'il s'agit d'un chiffre
+if [ -z "$1" ] || ! [[ "$1" =~ ^[0-9]+$ ]]; then
+    echo "Erreur: L'argument 'index' est obligatoire et doit être un chiffre."
+    echo "Utilisation: $0 <index>"
+    exit 1
+fi
+
+# Affecter l'argument à la variable index
+index="$1"
+
+# Lire toutes les clés dans le fichier experience_${index}.json en conservant l'ordre d'apparition
+keys=$(jq -r 'to_entries|map(.key)|.[]' "$base_dir/experience_${index}.json")
 
 # Boucler sur chaque clé et exécuter la commande
 for key in $keys
 do
-    echo "Lancement de la commande pour la clé: $key"
-    python3 "$base_dir/main.py" generate_configs "$key"
+    python3 "$base_dir/main.py" generate_configs "$index" "$key"
 done
--- other_files/repair_db.sh
+++ other_files/repair_db.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-dir="$GLOBALSCRATCH/generation"
-db_file="$dir/experience.db"
-db_save="$dir/experience_old.db"
-mv $db_file $db_save
+# Vérifier que l'argument index est fourni et qu'il s'agit d'un chiffre
+if [ -z "$1" ] || ! [[ "$1" =~ ^[0-9]+$ ]]; then
+    echo "Erreur: L'argument 'index' est obligatoire et doit être un chiffre."
+    echo "Utilisation: $0 <index>"
+    exit 1
+fi
 
+# Affecter l'argument à la variable index
+index="$1"
+
+# Définir le chemin de base pour les scripts et les données
+dir="$GLOBALSCRATCH/generation"
+
+# Utiliser l'index pour définir les fichiers de base de données
+db_file="$dir/experience_${index}.db"
+db_save="$dir/experience_old_${index}.db"
+
+# Renommer l'ancien fichier de base de données
+mv "$db_file" "$db_save"
+
+# Créer un nouveau fichier de base de données à partir de l'ancien
 cat <( sqlite3 "$db_save" .dump | grep "^ROLLBACK" -v ) <( echo "COMMIT;" ) | sqlite3 "$db_file"
